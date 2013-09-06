95f806a90724efd66cff1b071721cefc080bd608
Alexis Lecanu <alexis.lecanu@gmail.com>
2013-09-06 20:59:54 +0200

    BackgroundGIFSwipe
    Create a background which move with the offset and use a texture gif

---
+++ src/fr/ravenfeld/livewallpaper/library/objects/simple/BackgroundGIFSwipe.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2013 Alexis Lecanu
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package fr.ravenfeld.livewallpaper.library.objects.simple;
+
+import rajawali.materials.textures.ATexture.TextureException;
+
+public class BackgroundGIFSwipe extends BackgroundGIFFixed {
+	private float mWidthSwipe;
+
+	public BackgroundGIFSwipe(String nameTexture, int resourceId)
+			throws TextureException {
+		super(nameTexture, resourceId);
+	}
+
+	public BackgroundGIFSwipe(String nameTexture, int resourceID,
+			int textureSize) throws TextureException {
+		super(nameTexture, resourceID, textureSize);
+	}
+
+	@Override
+	public void surfaceChanged(int width, int height) {
+		float ratioDisplay = (float) height / (float) width;
+		float ratioSize = 1f / getHeight();
+		float scaleX = getWidth() * ratioSize * ratioDisplay;
+		mPlane.setScaleX(scaleX);
+		mPlane.setScaleY(1);
+
+		if (ratioDisplay >= 1) {
+			mWidthSwipe = scaleX;
+		} else {
+			mWidthSwipe = 1;
+		}
+	}
+
+	public void offsetsChanged(float xOffset) {
+		mPlane.setX((1 - mWidthSwipe) * (xOffset - 0.5f));
+	}
+
+}
